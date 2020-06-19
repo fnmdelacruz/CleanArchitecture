df76a48221f3406714229a1a3142619e867cc4b4
fnmdelacruz <fnmdelacruz@gmail.com>
2020-06-19 18:05:04 +0800

    Add API Project and Rename COntroller

---
+++ CleanArchitecture.API/Controllers/CourseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+    }
+}
